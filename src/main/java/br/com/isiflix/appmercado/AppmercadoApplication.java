fde08d340f1f3459cbb8646e13922d865b1deb3c
celsobritodev <celsinhobrito@gmail.com>
2025-11-08 20:59:36 -0300

    colocado banco e comandos postman

--- src/main/java/br/com/isiflix/appmercado/AppmercadoApplication.java
+++ src/main/java/br/com/isiflix/appmercado/AppmercadoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+// SOFTWARE DO INICIO AO FIM | | EPISÓDIO 5 - INSTALANDO SUA APLICAÇÃO EM NUVEM
+// https://youtu.be/-tSULL-a8Gc?list=PLjcmNukBom6-ChBclSAKTxP-HLZhVH2iO
 
 @SpringBootApplication
 public class AppmercadoApplication {
