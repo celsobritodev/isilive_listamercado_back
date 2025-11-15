e2b847d444c2abe29d143ba6811e9572c6055634
celsobritodev <celsinhobrito@gmail.com>
2025-11-15 18:03:44 -0300

    versao que vai 'subir'

--- src/main/java/br/com/isiflix/appmercado/AppmercadoApplication.java
+++ src/main/java/br/com/isiflix/appmercado/AppmercadoApplication.java
@@ -4,6 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+// usuaro e senha mysql
+
+//userlista
+//'Us3RList@'
+
+// usuario e senha vm
+// Usuario: devuser
+// Senha: java2025
+
+
 // SOFTWARE DO INICIO AO FIM | | EPISÓDIO 5 - INSTALANDO SUA APLICAÇÃO EM NUVEM
 // https://youtu.be/-tSULL-a8Gc?list=PLjcmNukBom6-ChBclSAKTxP-HLZhVH2iO
 
